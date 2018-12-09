61d78dea932c3a68898a4497adb4fe758406e711
DESKTOP-KMR7RKV\qxloo <573793638@qq.com>
2018-12-09 21:25:45 +0800

    add

--- shiro-spring-boot-starter/src/main/java/com/maxplus1/access/starter/config/shiro/ShiroAutoConfiguration.java
+++ shiro-spring-boot-starter/src/main/java/com/maxplus1/access/starter/config/shiro/ShiroAutoConfiguration.java
@@ -12,7 +12,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import javax.servlet.Filter;
 import java.util.HashMap;
